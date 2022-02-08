22f08aeccea3bf85609ed2c097086ccfb1a02f15
CodingLandx <98751849+CodingLandx@users.noreply.github.com>
2022-02-08 23:09:31 +0200

    Add files via upload

---
+++ btn.js
@@ -0,0 +1,68 @@
+"use strict";
+
+const btn1 = document.querySelector(".btn--1");
+const btn2 = document.querySelector(".btn--2");
+const btn3 = document.querySelector(".btn--3");
+const btn4 = document.querySelector(".btn--4");
+const btn5 = document.querySelector(".btn--5");
+const btn6 = document.querySelector(".btn--6");
+const btn7 = document.querySelector(".btn--7");
+const btn8 = document.querySelector(".btn--8");
+const btn9 = document.querySelector(".btn--9");
+const btn10 = document.querySelector(".btn--10");
+const btn11 = document.querySelector(".btn--11");
+const btn12 = document.querySelector(".btn--12");
+
+//Links
+
+btn1.addEventListener("click", function () {
+  window.open(
+    "https://www.youtube.com/watch?v=v8Gym_Fp4yo&list=PLX2nboN_I954eo5_I_pEPtFxeGWR9fKMl"
+  );
+});
+
+btn2.addEventListener("click", function () {
+  window.open("https://www.twitch.tv/syllabearretrocycles");
+});
+
+btn3.addEventListener("click", function () {
+  window.open("https://discord.gg/dcpaauj");
+});
+
+btn4.addEventListener("click", function () {
+  window.open("http://resource.armagetronad.net/resource/");
+});
+
+btn5.addEventListener("click", function () {
+  window.open(
+    "http://wiki.armagetronad.org/index.php?title=Ladle/Challenge_Board"
+  );
+});
+
+btn6.addEventListener("click", function () {
+  window.open("https://armarankings.com/");
+});
+
+btn7.addEventListener("click", function () {
+  window.open("http://wiki.armagetronad.org/index.php?title=Sumo");
+});
+
+btn8.addEventListener("click", function () {
+  window.open("http://wiki.armagetronad.net/index.php/Fortress");
+});
+
+btn9.addEventListener("click", function () {
+  window.open("https://retrocycles.net/");
+});
+
+btn10.addEventListener("click", function () {
+  window.open("http://wiki.armagetronad.org/index.php?title=Main_Page");
+});
+
+btn11.addEventListener("click", function () {
+  window.open("https://forums3.armagetronad.net/");
+});
+
+btn12.addEventListener("click", function () {
+  window.open("https://www.armanelgtron.tk/");
+});
