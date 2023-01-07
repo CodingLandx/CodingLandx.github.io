3f4243f721ffc65030a52999637e618ad5ef2176
CodingLandx <98751849+CodingLandx@users.noreply.github.com>
2023-01-07 03:51:46 +0000

    Add files via upload

---
+++ btn1.js
@@ -0,0 +1,27 @@
+"use strict";
+
+
+const btn12 = document.querySelector(".btn--12");
+const btn13 = document.querySelector(".btn--13");
+const btn14 = document.querySelector(".btn--14");
+const btn15 = document.querySelector(".btn--15");
+const btn16 = document.querySelector(".btn--16");
+
+//Links
+
+
+btn13.addEventListener("click", function () {
+    window.open("https://wiki.armagetronad.org/index.php/Ladle/Challenge_Board");
+  });
+
+  btn14.addEventListener("click", function () {
+    window.open("https://wiki.armagetronad.org/index.php?title=Ladle/Results");
+  });
+
+  btn15.addEventListener("click", function () {
+    window.open("https://wiki.armagetronad.org/index.php/Ladle/Challenge_Board#Recordings.2FScreenshots");
+  });
+
+  btn16.addEventListener("click", function () {
+    window.open("https://codinglandx.github.io/");
+  });
