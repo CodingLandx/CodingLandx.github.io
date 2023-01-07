dee15a8ea3d8c70cd260ffb54906f6c53d5c9a12
CodingLandx <98751849+CodingLandx@users.noreply.github.com>
2023-01-07 03:54:30 +0000

    Update btn1.js

--- btn1.js
+++ btn1.js
@@ -1,6 +1,16 @@
 "use strict";
 
-
+const btn1 = document.querySelector(".btn--1");
+const btn2 = document.querySelector(".btn--2");
+const btn3 = document.querySelector(".btn--3");
+const btn4 = document.querySelector(".btn--4");
+const btn5 = document.querySelector(".btn--5");
+const btn6 = document.querySelector(".btn--6");
+const btn7 = document.querySelector(".btn--7");
+const btn8 = document.querySelector(".btn--8");
+const btn9 = document.querySelector(".btn--9");
+const btn10 = document.querySelector(".btn--10");
+const btn11 = document.querySelector(".btn--11");
 const btn12 = document.querySelector(".btn--12");
 const btn13 = document.querySelector(".btn--13");
 const btn14 = document.querySelector(".btn--14");
@@ -9,19 +19,70 @@ const btn16 = document.querySelector(".btn--16");
 
 //Links
 
+if(btn1) btn1.addEventListener("click", function () {
+  window.open(
+    "https://www.youtube.com/watch?v=v8Gym_Fp4yo&list=PLX2nboN_I954eo5_I_pEPtFxeGWR9fKMl"
+  );
+});
 
-btn13.addEventListener("click", function () {
+if(btn2) btn2.addEventListener("click", function () {
+  window.open("https://www.twitch.tv/syllabearretrocycles");
+});
+
+if(btn3) btn3.addEventListener("click", function () {
+  window.open("https://discord.gg/dcpaauj");
+});
+
+if(btn4) btn4.addEventListener("click", function () {
+  window.open("http://resource.armagetronad.net/resource/");
+});
+
+if(btn5) btn5.addEventListener("click", function () {
+  window.open(
+    "http://wiki.armagetronad.org/index.php?title=Ladle/Challenge_Board"
+  );
+});
+
+if(btn6) btn6.addEventListener("click", function () {
+  window.open("https://armarankings.com/");
+});
+
+if(btn7) btn7.addEventListener("click", function () {
+  window.open("http://wiki.armagetronad.org/index.php?title=Sumo");
+});
+
+if(btn8) btn8.addEventListener("click", function () {
+  window.open("http://wiki.armagetronad.net/index.php/Fortress");
+});
+
+if(btn9) btn9.addEventListener("click", function () {
+  window.open("https://retrocycles.net/");
+});
+
+if(btn10) btn10.addEventListener("click", function () {
+  window.open("http://wiki.armagetronad.org/index.php?title=Main_Page");
+});
+
+if(btn11) btn11.addEventListener("click", function () {
+  window.open("https://forums3.armagetronad.net/");
+});
+
+if(btn12) btn12.addEventListener("click", function () {
+  window.open("https://www.armanelgtron.tk/");
+});
+
+if(btn13) btn13.addEventListener("click", function () {
     window.open("https://wiki.armagetronad.org/index.php/Ladle/Challenge_Board");
   });
 
-  btn14.addEventListener("click", function () {
+if(btn14) btn14.addEventListener("click", function () {
     window.open("https://wiki.armagetronad.org/index.php?title=Ladle/Results");
   });
 
-  btn15.addEventListener("click", function () {
+if(btn15) btn15.addEventListener("click", function () {
     window.open("https://wiki.armagetronad.org/index.php/Ladle/Challenge_Board#Recordings.2FScreenshots");
   });
 
-  btn16.addEventListener("click", function () {
+if(btn16) btn16.addEventListener("click", function () {
     window.open("https://codinglandx.github.io/");
   });
